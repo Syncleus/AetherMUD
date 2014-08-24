4e4cfca316dc97a499cec2fcf8437b274a3b5b79
Chris Kearney <chris@kearneymail.com>
2014-08-23 20:17:52 -0700

    formatting

--- src/main/java/com/comandante/creeper/Main.java
+++ src/main/java/com/comandante/creeper/Main.java
@@ -42,22 +42,17 @@ public class Main {
         RoomManager roomManager = new RoomManager();
         EntityManager entityManager = new EntityManager(roomManager);
         GameManager gameManager = new GameManager(roomManager, playerManager, entityManager);
-        entityManager.addEntity(new BasicRoom(1, "Lobby", Optional.of(2), Optional.<Integer>absent(), Optional.<Integer>absent(), Optional.<Integer>absent(), Optional.<Integer>absent(), Optional.<Integer>absent(),
+        entityManager.addEntity(new BasicRoom(
+                1,
+                "Lobby",
+                Optional.of(2),
+                Optional.<Integer>absent(),
+                Optional.<Integer>absent(),
+                Optional.<Integer>absent(),
+                Optional.<Integer>absent(),
+                Optional.<Integer>absent(),
                 "This is the lobby. It's pretty empty and the paint still smells fresh.\r\n"));
-        entityManager.addEntity(new BasicRoom(2, "Hallway", Optional.of(3), Optional.<Integer>absent(), Optional.<Integer>absent(), Optional.of(1), Optional.<Integer>absent(), Optional.<Integer>absent(),
-                "This is the hallway. It's long and hallway-ish with exposed wires and floorboards showing.\r\n"));
-        entityManager.addEntity(new BasicRoom(3, "Intake", Optional.<Integer>absent(), Optional.of(6), Optional.<Integer>absent(), Optional.of(2),Optional.<Integer>absent(), Optional.<Integer>absent(),
-                "This is the intake area.  People are lined up like cattle waiting to be prodded.\r\n"));
-        entityManager.addEntity(new BasicRoom(6, "Closet", Optional.<Integer>absent(), Optional.<Integer>absent(), Optional.of(3), Optional.of(7), Optional.<Integer>absent(), Optional.<Integer>absent(),
-                "You find yourself in the janitorial closet.  It smells like bleach.\r\n"));
-        entityManager.addEntity(new BasicRoom(7, "Toilet", Optional.of(6), Optional.<Integer>absent(), Optional.<Integer>absent(), Optional.<Integer>absent(), Optional.of(8), Optional.<Integer>absent(),
-                "You find yourself in the toilet. The smell is horrible. There's an opening in the ceiling.\r\n"));
-        entityManager.addEntity(new BasicRoom(8, "Toilet Ceiling Crawl Space", Optional.<Integer>absent(), Optional.<Integer>absent(), Optional.<Integer>absent(), Optional.<Integer>absent(),Optional.<Integer>absent(), Optional.of(7),
-                "It's cramped as all hell up here.  By the looks of it someone has been spending a great deal of time up here.\r\n"));
         entityManager.addEntity(new Derper(gameManager, 1));
-        entityManager.addEntity(new Derper(gameManager, 1));
-        entityManager.addEntity(new Derper(gameManager, 7));
-        entityManager.addEntity(new Derper(gameManager, 7));
         CommandService commandService = new CommandService();
         DefaultCommandHandler defaultCommandHandler = new DefaultCommandHandler(gameManager, commandService);
 
