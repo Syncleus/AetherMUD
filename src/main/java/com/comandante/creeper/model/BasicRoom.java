0c8112c1a6f1709d1d3e542978314cc2424f1b01
Chris Kearney <chris@kearneymail.com>
2014-08-23 20:04:48 -0700

    registering rooms as creeperentities so they get a tick

--- src/main/java/com/comandante/creeper/model/BasicRoom.java
+++ src/main/java/com/comandante/creeper/model/BasicRoom.java
@@ -3,6 +3,7 @@ package com.comandante.creeper.model;
 import com.google.common.base.Optional;
 
 public class BasicRoom extends Room {
+
     public BasicRoom(Integer roomId, String roomTitle, Optional<Integer> northId, Optional<Integer> westId, Optional<Integer> eastId, Optional<Integer> southId, Optional<Integer> upId, Optional<Integer> downId, String roomDescription) {
         super(roomId, roomTitle, northId, westId, eastId, southId, upId, downId, roomDescription);
     }
