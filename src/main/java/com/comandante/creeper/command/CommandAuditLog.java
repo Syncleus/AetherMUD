6d69c38b46d89ef88b1c484714f22f67c152e097
Chris Kearney <chris@kearneymail.com>
2015-04-28 14:45:39 -0700

    admin role, log files

---
+++ src/main/java/com/comandante/creeper/command/CommandAuditLog.java
@@ -0,0 +1,7 @@
+package com.comandante.creeper.command;
+
+/**
+ * Created by kearney on 4/28/15.
+ */
+public class CommandAuditLog {
+}
--- src/main/java/com/comandante/creeper/player/PlayerRole.java
+++ src/main/java/com/comandante/creeper/player/PlayerRole.java
@@ -1,13 +1,15 @@
 package com.comandante.creeper.player;
 
-public enum PlayerRoles {
+public enum PlayerRole {
 
-    ADMIN("administrator", 1);
+    ADMIN("administrator", 1),
+    MORTAL("mortal", 2);
+
 
     private final String roleType;
     private final int roleId;
 
-    PlayerRoles(String roleType, int roleId) {
+    PlayerRole(String roleType, int roleId) {
         this.roleType = roleType;
         this.roleId = roleId;
     }
