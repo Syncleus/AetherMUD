7a0cca428efd83c891944df0e17a516d5a1c96dd
Chris Kearney <chris@kearneymail.com>
2015-05-24 20:21:16 -0700

    rollup on this inventory command

--- src/main/java/com/comandante/creeper/command/InventoryCommand.java
+++ src/main/java/com/comandante/creeper/command/InventoryCommand.java
@@ -4,12 +4,13 @@ package com.comandante.creeper.command;
 import com.comandante.creeper.Items.Item;
 import com.comandante.creeper.Items.ItemType;
 import com.comandante.creeper.managers.GameManager;
-import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.MessageEvent;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static com.comandante.creeper.server.Color.RESET;
 
@@ -32,25 +33,39 @@ public class InventoryCommand extends Command {
                 write("You aren't carrying anything.");
                 return;
             }
-            StringBuilder sb = new StringBuilder();
-            sb.append("You are carrying:\r\n");
-            sb.append(RESET);
-            List<String> invStrings = Lists.newArrayList();
+            StringBuilder inventoryString = new StringBuilder();
+            inventoryString.append("You are carrying:\r\n");
+            inventoryString.append(RESET);
+            Map<String, Integer> itemAndCounts = Maps.newHashMap();
             for (Item item : inventory) {
-                sb.append(item.getItemName());
+                StringBuilder invItem = new StringBuilder();
+                invItem.append(item.getItemName());
                 int maxUses = ItemType.itemTypeFromCode(item.getItemTypeId()).getMaxUses();
                 if (maxUses > 0) {
                     int remainingUses = maxUses - item.getNumberOfUses();
-                    sb.append(" - ").append(remainingUses);
+                    invItem.append(" - ").append(remainingUses);
                     if (remainingUses == 1) {
-                        sb.append(" use left.");
+                        invItem.append(" use left.");
                     } else {
-                        sb.append(" uses left.");
+                        invItem.append(" uses left.");
                     }
                 }
-                sb.append("\r\n");
+                if (itemAndCounts.containsKey(invItem.toString())) {
+                    Integer integer = itemAndCounts.get(invItem.toString());
+                    integer = integer + 1;
+                    itemAndCounts.put(invItem.toString(), integer);
+                } else {
+                    itemAndCounts.put(invItem.toString(), 1);
+                }
             }
-            write(sb.toString());
+            for (Map.Entry<String, Integer> next : itemAndCounts.entrySet()) {
+                if (next.getValue() > 1) {
+                    inventoryString.append(next.getKey()).append(" (").append(next.getValue()).append(")").append("\r\n");
+                } else {
+                    inventoryString.append(next.getKey()).append("\r\n");
+                }
+            }
+            write(inventoryString.toString());
         } finally {
             super.messageReceived(ctx, e);
         }
