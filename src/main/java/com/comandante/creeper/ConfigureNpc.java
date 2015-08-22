c2e3d6b83ef9324d08233c9298db825549fac93a
BLAZECRAFT <n3vs@yahoo.com>
2015-08-22 01:50:07 -0400

    Update ConfigureNpc.java

--- src/main/java/com/comandante/creeper/ConfigureNpc.java
+++ src/main/java/com/comandante/creeper/ConfigureNpc.java
@@ -66,6 +66,12 @@ public class ConfigureNpc {
 
         LloydBartender lloydBartender = new LloydBartender(gameManager, new Loot(18, 26, Sets.<ItemType>newHashSet()), itemsForSale);
         gameManager.getRoomManager().addMerchant(64, lloydBartender);
+        
+        Map<Integer, MerchantItemForSale> nigelBartender = Maps.newLinkedHashMap();
+        nigelBartender.put(1, new MerchantItemForSale(ItemType.BEER, 6));
+        
+        NigelBartender nigelBartender = new NigelBartender(gameManager, new Loot(18, 26, Sets.<ItemType>newHashSet()), itemsForSale);
+        gameManager.getRoomManager().addMerchant(377, nigelBartender);
 
         Map<Integer, MerchantItemForSale> blacksmithItems = Maps.newHashMap();
         blacksmithItems.put(1, new MerchantItemForSale(ItemType.BROAD_SWORD, 1000));
