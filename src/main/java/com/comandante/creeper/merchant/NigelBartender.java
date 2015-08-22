0b27689580e659b13020e61b7a8546a6a11f6001
BLAZECRAFT <n3vs@yahoo.com>
2015-08-22 01:56:53 -0400

    Create NigelBartender.java

---
+++ src/main/java/com/comandante/creeper/merchant/NigelBartender.java
@@ -0,0 +1,27 @@
+package com.comandante.creeper.merchant;
+
+import com.comandante.creeper.Items.Loot;
+import com.comandante.creeper.managers.GameManager;
+import com.comandante.creeper.server.Color;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static com.comandante.creeper.server.Color.BOLD_ON;
+
+public class NigelBartender extends Merchant {
+    private final static long phraseIntervalMs = 300000;
+    private final static String NAME = "nigel the bartender";
+    private final static String welcomeMessage = " N I G E L 'S   B A R ";
+    private final static Set<String> validTriggers = new HashSet<String>(Arrays.asList(new String[]
+                    {"nigel", "bartender", "barkeep", "Nigel", "NIGEL", NAME}
+    ));
+
+    private final static String colorName = BOLD_ON + Color.CYAN + NAME  + Color.RESET ;
+
+    public NigelBartender(GameManager gameManager, Loot loot, Map<Integer, MerchantItemForSale> merchantItemForSales) {
+        super(gameManager, NAME, colorName, validTriggers, merchantItemForSales, welcomeMessage);
+    }
+}
