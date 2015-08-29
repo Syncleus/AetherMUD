4980ba2f1feb46a1bddd8aaeec4bd7e298ebda19
BLAZECRAFT <n3vs@yahoo.com>
2015-08-28 20:30:54 -0400

    Create WentworthTailor.java

---
+++ src/main/java/com/comandante/creeper/merchant/WentworthTailor.java
@@ -0,0 +1,27 @@
+package com.comandante.creeper.merchant;
+
+import com.comandante.creeper.Items.Loot;
+import com.comandante.creeper.managers.GameManager;
+import com.comandante.creeper.server.Color;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static com.comandante.creeper.server.Color.BOLD_ON;
+
+public class WentworthTailor extends Merchant {
+    private final static long phraseIntervalMs = 300000;
+    private final static String NAME = "wentworth";
+    private final static String welcomeMessage = "Welcome to WENTWORTH's TAILOR SHOP.\r\n";
+    private final static Set<String> validTriggers = new HashSet<String>(Arrays.asList(new String[]
+                    {"wentworth", "w", "tailor", "t", NAME}
+    ));
+
+    private final static String colorName = BOLD_ON + Color.CYAN + NAME  + Color.RESET ;
+
+    public WentworthTailor(GameManager gameManager, Loot loot, Map<Integer, MerchantItemForSale> merchantItemForSales) {
+        super(gameManager, NAME, colorName, validTriggers, merchantItemForSales, welcomeMessage);
+    }
+}
