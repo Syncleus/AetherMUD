08d5021f3b93f579c6d7b6f71cdb02daa4afd9f0
Chris Kearney <chris@kearneymail.com>
2015-10-21 11:09:05 -0700

    one more who cmmand fix

--- src/main/java/com/comandante/creeper/command/WhoCommand.java
+++ src/main/java/com/comandante/creeper/command/WhoCommand.java
@@ -35,7 +35,7 @@ public class WhoCommand extends Command {
                     ShownBorders.NONE);
             t.setColumnWidth(0, 14, 24);
             t.setColumnWidth(1, 7, 7);
-            t.setColumnWidth(2, 15, 22);
+            t.setColumnWidth(2, 16, 16);
             t.addCell("Player");
             t.addCell("Level");
             t.addCell("XP");
