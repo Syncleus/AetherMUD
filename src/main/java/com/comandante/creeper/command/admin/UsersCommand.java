d7f919dfd8fa91fe23ec190663ebb4856d831d77
Chris Kearney <chris@kearneymail.com>
2016-08-07 16:18:20 -0700

    a stackoverflow solution to printing friendly idle time

--- src/main/java/com/comandante/creeper/command/admin/UsersCommand.java
+++ src/main/java/com/comandante/creeper/command/admin/UsersCommand.java
@@ -15,10 +15,7 @@ import org.nocrala.tools.texttablefmt.Table;
 import java.net.InetSocketAddress;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class UsersCommand extends Command {
@@ -52,20 +49,94 @@ public class UsersCommand extends Command {
                 String remoteUsersHost = remoteAddress.getHostString();
                 t.addCell(remoteUsersHost);
 
-                DateFormat format = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-                String loginTime = format.format(new Date(playerManager.getSessionManager().getSession(allPlayer.getPlayerId()).getInitialLoginTime()));
+                SimpleDateFormat loggedInFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+                String loginTime = loggedInFormat.format(new Date(playerManager.getSessionManager().getSession(allPlayer.getPlayerId()).getInitialLoginTime()));
                 t.addCell(loginTime);
 
                 long lastActivity = playerManager.getSessionManager().getSession(allPlayer.getPlayerId()).getLastActivity();
-                long idleTimeMs = System.currentTimeMillis() - lastActivity;
-                String idleTime = String.format("%d min, %d sec",
-                        TimeUnit.MILLISECONDS.toMinutes(idleTimeMs),
-                        TimeUnit.MILLISECONDS.toSeconds(idleTimeMs) -
-                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(idleTimeMs))
-                );
+                String idleTime = getFriendlyTime(new Date(lastActivity));
                 t.addCell(idleTime);
             }
             write(t.render());
         });
+    }
+
+    public static String getFriendlyTime(Date dateTime) {
+        StringBuffer sb = new StringBuffer();
+        Date current = Calendar.getInstance().getTime();
+        long diffInSeconds = (current.getTime() - dateTime.getTime()) / 1000;
+
+        long sec = (diffInSeconds >= 60 ? diffInSeconds % 60 : diffInSeconds);
+        long min = (diffInSeconds = (diffInSeconds / 60)) >= 60 ? diffInSeconds % 60 : diffInSeconds;
+        long hrs = (diffInSeconds = (diffInSeconds / 60)) >= 24 ? diffInSeconds % 24 : diffInSeconds;
+        long days = (diffInSeconds = (diffInSeconds / 24)) >= 30 ? diffInSeconds % 30 : diffInSeconds;
+        long months = (diffInSeconds = (diffInSeconds / 30)) >= 12 ? diffInSeconds % 12 : diffInSeconds;
+        long years = (diffInSeconds = (diffInSeconds / 12));
+
+        if (years > 0) {
+            if (years == 1) {
+                sb.append("a year");
+            } else {
+                sb.append(years + " years");
+            }
+            if (years <= 6 && months > 0) {
+                if (months == 1) {
+                    sb.append(" and a month");
+                } else {
+                    sb.append(" and " + months + " months");
+                }
+            }
+        } else if (months > 0) {
+            if (months == 1) {
+                sb.append("a month");
+            } else {
+                sb.append(months + " months");
+            }
+            if (months <= 6 && days > 0) {
+                if (days == 1) {
+                    sb.append(" and a day");
+                } else {
+                    sb.append(" and " + days + " days");
+                }
+            }
+        } else if (days > 0) {
+            if (days == 1) {
+                sb.append("a day");
+            } else {
+                sb.append(days + " days");
+            }
+            if (days <= 3 && hrs > 0) {
+                if (hrs == 1) {
+                    sb.append(" and an hour");
+                } else {
+                    sb.append(" and " + hrs + " hours");
+                }
+            }
+        } else if (hrs > 0) {
+            if (hrs == 1) {
+                sb.append("an hour");
+            } else {
+                sb.append(hrs + " hours");
+            }
+            if (min > 1) {
+                sb.append(" and " + min + " minutes");
+            }
+        } else if (min > 0) {
+            if (min == 1) {
+                sb.append("a minute");
+            } else {
+                sb.append(min + " minutes");
+            }
+            if (sec > 1) {
+                sb.append(" and " + sec + " seconds");
+            }
+        } else {
+            if (sec <= 1) {
+                sb.append("about a second");
+            } else {
+                sb.append("about " + sec + " seconds");
+            }
+        }
+        return sb.toString();
     }
 }
