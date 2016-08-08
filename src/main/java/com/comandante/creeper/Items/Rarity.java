787dcd00d08bbd681fbb181a27e523468f3656ad
Chris Kearney <chris@kearneymail.com>
2016-08-07 23:55:55 -0700

    often

--- src/main/java/com/comandante/creeper/Items/Rarity.java
+++ src/main/java/com/comandante/creeper/Items/Rarity.java
@@ -3,7 +3,7 @@ package com.comandante.creeper.Items;
 
 public enum Rarity {
 
-    ALWAYS("always", 80),
+    OFTEN("often", 40),
     BASIC("basic", 15.0),
     UNCOMMON("uncommon", 7),
     RARE("rare", 3),
