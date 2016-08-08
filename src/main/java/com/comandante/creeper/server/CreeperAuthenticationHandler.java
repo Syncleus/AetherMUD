e86c14d63547ab71e017f57e716b627716d1ffe4
Chris Kearney <chris@kearneymail.com>
2016-08-07 22:44:46 -0700

    new logo

--- src/main/java/com/comandante/creeper/server/CreeperAuthenticationHandler.java
+++ src/main/java/com/comandante/creeper/server/CreeperAuthenticationHandler.java
@@ -14,28 +14,29 @@ public class CreeperAuthenticationHandler extends SimpleChannelUpstreamHandler {
     private final GameManager gameManager;
     private final CreeperAuthenticator creeperAuthenticator;
     private static final Logger log = Logger.getLogger(CreeperAuthenticationHandler.class);
-    private static final String LOGO = "                          /[-])//  ___\n\r" +
-            "                     __ --\\ `_/~--|  / \\\n\r" +
-            "                   /_-/~~--~~ /~~~\\\\_\\ /\\\n\r" +
-            "                   |  |___|===|_-- | \\ \\ \\\n\r" +
-            " _/~~~~~~~~|~~\\,   ---|---\\___/----|  \\/\\-\\\n\r" +
-            " ~\\________|__/   / // \\__ |  ||  / | |   | |\n\r" +
-            "          ,~-|~~~~~\\--, | \\|--|/~|||  |   | |\n\r" +
-            "          [3-|____---~~ _--'==;/ _,   |   |_|\n\r" +
-            "                      /   /\\__|_/  \\  \\__/--/\n\r" +
-            "                     /---/_\\  -___/ |  /,--|\n\r" +
-            "                     /  /\\/~--|   | |  \\///\n\r" +
-            "                    /  / |-__ \\    |/\n\r" +
-            "                   |--/ /      |-- | \\\n\r" +
-            "                  \\^~~\\\\/\\      \\   \\/- _\n\r" +
-            "                   \\    |  \\     |~~\\~~| \\\n\r" +
-            "                    \\    \\  \\     \\   \\  | \\\n\r" +
-            "                      \\    \\ |     \\   \\    \\\n\r" +
-            "                       |~~|\\/\\|     \\   \\   |\n\r" +
-            "                      |   |/         \\_--_- |\\\n\r" +
-            "                      |  /            /   |/\\/\n\r" +
-            "                       ~~             /  /\n\r" +
-            "                                     |__/\n\r";
+    private static final String LOGO = "                      __gggrgM**M#mggg__\n" +
+            "                 __wgNN@\"B*P\"\"mp\"\"@d#\"@N#Nw__\n" +
+            "               _g#@0F_a*F#  _*F9m_ ,F9*__9NG#g_\n" +
+            "            _mN#F  aM\"    #p\"    !q@    9NL \"9#Qu_\n" +
+            "           g#MF _pP\"L  _g@\"9L_  _g\"\"#__  g\"9w_ 0N#p\n" +
+            "         _0F jL*\"   7_wF     #_gF     9gjF   \"bJ  9h_\n" +
+            "        j#  gAF    _@NL     _g@#_      J@u_    2#_  #_\n" +
+            "       ,FF_#\" 9_ _#\"  \"b_  g@   \"hg  _#\"  !q_ jF \"*_09_\n" +
+            "       F N\"    #p\"      Ng@       `#g\"      \"w@    \"# t\n" +
+            "      j p#    g\"9_     g@\"9_      gP\"#_     gF\"q    Pb L\n" +
+            "      0J  k _@   9g_ j#\"   \"b_  j#\"   \"b_ _d\"   q_ g  ##\n" +
+            "      #F  `NF     \"#g\"       \"Md\"       5N#      9W\"  j#\n" +
+            "      #k  jFb_    g@\"q_     _*\"9m_     _*\"R_    _#Np  J#\n" +
+            "      tApjF  9g  J\"   9M_ _m\"    9%_ _*\"   \"#  gF  9_jNF\n" +
+            "       k`N    \"q#       9g@        #gF       ##\"    #\"j\n" +
+            "       `_0q_   #\"q_    _&\"9p_    _g\"`L_    _*\"#   jAF,'\n" +
+            "        9# \"b_j   \"b_ g\"    *g _gF    9_ g#\"  \"L_*\"qNF\n" +
+            "         \"b_ \"#_    \"NL      _B#      _I@     j#\" _#\"\n" +
+            "           NM_0\"*g_ j\"\"9u_  gP  q_  _w@ ]_ _g*\"F_g@\n" +
+            "            \"NNh_ !w#_   9#g\"    \"m*\"   _#*\" _dN@\"\n" +
+            "               9##g_0@q__ #\"4_  j*\"k __*NF_g#@P\"\n" +
+            "                 \"9NN#gIPNL_ \"b@\" _2M\"Lg#N@F\"\n" +
+            "                     \"\"P@*NN#gEZgNN@#@P\"\"\n";
 
     public CreeperAuthenticationHandler(GameManager gameManager) {
         this.gameManager = gameManager;
