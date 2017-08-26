07e230b54eadcc56732304886dc62175964c608a
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2017-08-26 19:03:47 -0400

    Updated the pom to the standard format, and added license headers, also fixed how the program reports its version.

--- src/main/java/com/comandante/creeper/Main.java
+++ src/main/java/com/comandante/creeper/Main.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2017 Syncleus, Inc.
+ * with portions copyright 2004-2017 Bo Zimmerman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.comandante.creeper;
 
 import com.codahale.metrics.JmxReporter;
@@ -42,25 +58,15 @@ public class Main {
     final public static Set<Character> vowels = new HashSet<Character>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
 
     public static String getCreeperVersion() {
-        Properties props = new Properties();
-        try {
-            props.load(Main.class.getResourceAsStream("/build.properties"));
-        } catch (Exception e) {
-            log.error("Problem reading build properties file.", e);
-            return "0";
-        }
-        String buildVersion = props.getProperty("build.version");
-        String timestampString = props.getProperty("build.timestamp");
-        if (buildVersion == null || timestampString == null || timestampString.isEmpty() || buildVersion.isEmpty()) {
+
+        String buildVersion = Main.class.getPackage().getSpecificationVersion();
+        if (buildVersion == null || buildVersion.isEmpty()) {
             return "creeper-local-dev";
         }
-        long buildTimestamp = Long.parseLong(timestampString);
-        Date date = new Date(buildTimestamp);
-        SimpleDateFormat format = new SimpleDateFormat();
-        String dateFormatted = format.format(date);
 
-        return buildVersion + " " + dateFormatted;
+        return buildVersion ;
     }
+
     public static void main(String[] args) throws Exception {
 
         CreeperConfiguration creeperConfiguration = new CreeperConfiguration(buildConfiguration(args));
